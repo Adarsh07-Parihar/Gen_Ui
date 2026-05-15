d2c473ee207a73048bd4858dcafd33cd91600430
Adarsh07-Parihar <pariharmanas85@gmail.com>
2026-05-15 18:18:13 +0530

    Update website.controllers.js

--- server/controllers/website.controllers.js
+++ server/controllers/website.controllers.js
@@ -354,8 +354,7 @@ export const deploy=async (req,res)=>{
 export const getBySlug=async (req,res) => {
     try {
         const website=await Website.findOne({
-            slug:req.params.slug,
-            user:req.user._id
+            slug:req.params.slug
         })
 
         if(!website){
