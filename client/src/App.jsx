1e427dac8356fbd412a276bec9ba1bdedefd83fc
Adarsh07-Parihar <pariharmanas85@gmail.com>
2026-05-15 17:21:18 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import Generate from './pages/Generate'
 import WebsiteEditor from './pages/Editor'
 import LiveSite from './pages/LiveSite'
 import Pricing from './pages/Pricing'
-export const serverUrl="http://localhost:8000"
+export const serverUrl="https://gen-ui-3sf8.onrender.com"
 
 const App = () => {
   useGetCurrentUser()
