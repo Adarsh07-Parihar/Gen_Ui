2c018e70a894500e61502b660506428cebe04b21
Adarsh07-Parihar <pariharmanas85@gmail.com>
2026-05-17 16:24:07 +0530

    Update openRouter.js

--- server/config/openRouter.js
+++ server/config/openRouter.js
@@ -1,33 +1,53 @@
-const openRouterUrl="https://openrouter.ai/api/v1/chat/completions"
-const model="deepseek/deepseek-chat"
+const openRouterUrl = "https://openrouter.ai/api/v1/chat/completions";
 
-export const generateResponse=async (prompt)=>{
-const res=await fetch(openRouterUrl, {
-  method: 'POST',
-  headers: {
-    Authorization: `Bearer ${process.env.OPEN_ROUTER_API_KEY}`,
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({
-    model: model,
-    messages: [
-        {role:"system",content:"You must return ONLY valid raw JSON."}
-        ,
-      {
-        role: 'user',
-        content: prompt,
-      },
-    ],
-    temperature:0.2
-  }),
-});
+const model = "deepseek/deepseek-chat";
 
-if(!res.ok){
-    const err=await res.text()
-    throw new Error ("openRouter err"+err)
-}
+export const generateResponse = async (prompt) => {
 
-const data=await res.json()
-return data.choices[0].message.content
+    const res = await fetch(openRouterUrl, {
 
-}
+        method: 'POST',
+
+        headers: {
+            Authorization: `Bearer ${process.env.OPEN_ROUTER_API_KEY}`,
+            'Content-Type': 'application/json',
+        },
+
+        body: JSON.stringify({
+
+            model: model,
+
+            messages: [
+
+                {
+                    role: "system",
+                    content:
+`Return ONLY valid JSON.
+Do not include markdown.
+Do not use triple backticks.
+Do not explain anything.`
+                },
+
+                {
+                    role: 'user',
+                    content: prompt,
+                },
+            ],
+
+            temperature: 0.2,
+
+            max_tokens: 4000
+        }),
+    });
+
+    if (!res.ok) {
+
+        const err = await res.text();
+
+        throw new Error("openRouter err " + err);
+    }
+
+    const data = await res.json();
+
+    return data.choices[0].message.content;
+};
